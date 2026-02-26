3904747b9a3ef13946e249821b0bdbefaad28ae4
usami <29851566+thisusami@users.noreply.github.com>
2026-02-26 22:59:06 +0700

    Update postgresql.go

--- postgresql.go
+++ postgresql.go
@@ -1,1 +1,31 @@
 
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func createDBPool(dsn string) *sql.DB {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	
+	db.SetMaxOpenConns(25)                 
+	db.SetMaxIdleConns(25)               
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxIdleTime(2 * time.Minute)
+
+	// Verify connection
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
